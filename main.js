2e858bf2b5c63500dc3873e07c9558c39f9e5820
Ted Lazaros <tedlaz@gmail.com>
2024-04-11 12:07:51 +0300

    refactoring css

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
-const template = document.querySelector("#pet-card-template")
+const template = document.querySelector("#episode-card-template")
 const wrapper = document.createDocumentFragment()
 
-async function start() {
-  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
-  const weatherData = await weatherPromise.json()
-
-  const ourTemperature = weatherData.properties.periods[0].temperature
-  document.querySelector("#temperature-output").textContent = ourTemperature
-
-}
 
 async function fetchEpisodes(showName) {
   const showPromise = await fetch(`https://api.tvmaze.com/search/shows?q=${showName}`)
@@ -24,27 +16,29 @@ async function fetchEpisodes(showName) {
 
   document.querySelector(".page-section-title").textContent = `${showName} episodes`
 
-  episodeData.forEach(episode => {
+  episodeData.forEach((episode) => {
     const clone = template.content.cloneNode(true)
-    clone.querySelector(".pet-card").dataset.species = 'dog'
+    clone.querySelector(".episode-card").dataset.species = "dog"
     clone.querySelector("h3").textContent = episode.name
-    clone.querySelector(".pet-description").innerHTML = episode.summary
-    clone.querySelector(".pet-age").textContent = `Season ${episode.season} episode ${episode.number} (${episode.airdate})`
-    clone.querySelector(".pet-card-photo img").src = episode.image ? episode.image.original : "images/fallback.jpg"
-    clone.querySelector(".pet-card-photo img").alt = `episode ${episode.name}`
+    clone.querySelector(".episode-description").innerHTML = episode.summary
+    clone.querySelector(
+      ".episode-age"
+    ).textContent = `Season ${episode.season} episode ${episode.number} (${episode.airdate})`
+    clone.querySelector(".episode-card-photo img").src = episode.image
+      ? episode.image.original
+      : "images/fallback.jpg"
+    clone.querySelector(".episode-card-photo img").alt = `episode ${episode.name}`
     wrapper.appendChild(clone)
   })
 
-  document.querySelector(".list-of-pets").appendChild(wrapper)
-
+  document.querySelector(".list-of-episodes").appendChild(wrapper)
 }
 
+const fetchButton = document.getElementById("fetchButton")
+const searchInput = document.getElementById("searchInput")
 
-const fetchButton = document.getElementById('fetchButton')
-const searchInput = document.getElementById('searchInput')
-
-fetchButton.addEventListener('click', () => {
-  const listOfPets = document.querySelector(".list-of-pets")
-  listOfPets.innerHTML = ""
+fetchButton.addEventListener("click", () => {
+  const listOfEpisodes = document.querySelector(".list-of-episodes")
+  listOfEpisodes.innerHTML = ""
   fetchEpisodes(searchInput.value)
 })
